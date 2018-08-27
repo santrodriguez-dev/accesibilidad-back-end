73f59a5b6fd01a6fcb661072f1c32c30091bffb2
Santiago Rodríguez <roms_195@hotmail.com>
2018-08-26 23:09:58 -0500

    ...

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "production";
 const config = require(__dirname + "./../../config/config.json")[env];
 
 export const sequelize = new Sequelize(config.database, config.username, config.password, config);
